41b2aa6c28f3af782ca0fa42caf95c022927a22c
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-02-11 17:57:35 -0600

    Intial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-lime-50 to-red-50">
+      <h1 className="pt-4 text-center text-5xl text-black/60 bg-transparent">Sentiment Analysis</h1>
+      
+    </div>
+  );
+}
+
+export default App;
