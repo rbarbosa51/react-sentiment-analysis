b0eb2df8e30f6f34d02f6582cf3fb08fed015761
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-02-11 20:20:14 -0600

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-lime-50 to-red-50 text-black/60">
+      <div
+        ref={modal}
+        className="mod absolute hidden rounded-lg border-[2px] border-solid border-black/60 bg-white p-16 text-5xl"
+      >
+        Please wait...
+      </div>
       <h1 className="bg-transparent pt-4 text-center  text-5xl">
         Sentiment Analysis
       </h1>
