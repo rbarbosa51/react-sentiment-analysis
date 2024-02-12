67811b592a4cded537acf16fc7e42295b622fe28
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-02-11 20:17:48 -0600

    Reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { pipeline, env } from "@xenova/transformers";
+import { useState, useRef } from "react";
+
+env.allowLocalModels = false;
+env.useBrowserCache = false;
+
 function App() {
+  const [textInput, setTextInput] = useState("")
+  const [sentiment, setSentiment] = useState("Placeholder");
+  const [score, setScore] = useState("Placeholder");
+  const modal = useRef(null)
+
+  const handleInput = async () => {
+    modal.current.classList.toggle('hidden')
+    const pipe = await pipeline('sentiment-analysis');
+    const out = await pipe(textInput);
+    setSentiment(out[0].label)
+    setScore(out[0].score)
+    modal.current.classList.toggle('hidden')
+  }
+
   return (
-    <div className="min-h-screen bg-gradient-to-b from-lime-50 to-red-50 text-black/60">
+    <div className="min-h-screen bg-gradient-to-b from-lime-50 to-red-50 text-black/60 relative">
+      <div ref={modal} className="mod hidden absolute p-16 text-5xl rounded-lg bg-white border-[2px] border-solid border-black/60">Please wait...</div>
       <h1 className="bg-transparent pt-4 text-center  text-5xl">
         Sentiment Analysis
       </h1>
@@ -10,13 +31,16 @@ function App() {
       </p>
       <div className="flex flex-col items-center justify-center gap-4">
         <textarea
-          className="w-[60%] rounded-lg border-[1px] border-solid border-black/60"
-          name=""
-          id=""
+          className="w-[60%] rounded-lg border-[1px] border-solid border-black/60 p-2"
+          name="textInput"
+          id="textInput"
           cols="30"
           rows="5"
+          onChange={(e) => setTextInput(e.target.value)}
         ></textarea>
-        <button className="rounded-lg bg-red-100 p-2 font-semibold duration-500 hover:scale-125">
+        <button className="rounded-lg bg-red-100 p-2 font-semibold duration-100 hover:scale-125"
+          onClick={handleInput}
+        >
           Analyse
         </button>
       </div>
@@ -24,9 +48,11 @@ function App() {
       <hr className="mx-auto my-4 w-[60%]" />
       <div className="flex justify-center gap-28">
         <div>Sentiment: </div>
-        <div>Placeholder</div>
+        {/* Sentiment State */}
+        <div>{sentiment}</div>
         <div>Score</div>
-        <div>Placeholder</div>
+        {/* Score State */}
+        <div>{score}</div>
       </div>
     </div>
   );
