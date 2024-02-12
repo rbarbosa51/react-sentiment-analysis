93f459004c166f02f1ad2edaa561f58acac64af6
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-02-11 18:54:04 -0600

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-lime-50 to-red-50">
-      <h1 className="pt-4 text-center text-5xl text-black/60 bg-transparent">Sentiment Analysis</h1>
-      
+    <div className="min-h-screen bg-gradient-to-b from-lime-50 to-red-50 text-black/60">
+      <h1 className="bg-transparent pt-4 text-center  text-5xl">
+        Sentiment Analysis
+      </h1>
+      <hr className="mx-auto mb-8 mt-2 w-[60%]" />
+      <p className="mx-auto my-2 w-96 text-center">
+        Write some text you wish to analyse:
+      </p>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <textarea
+          className="w-[60%] rounded-lg border-[1px] border-solid border-black/60"
+          name=""
+          id=""
+          cols="30"
+          rows="5"
+        ></textarea>
+        <button className="rounded-lg bg-red-100 p-2 font-semibold duration-500 hover:scale-125">
+          Analyse
+        </button>
+      </div>
+
+      <hr className="mx-auto my-4 w-[60%]" />
+      <div className="flex justify-center gap-28">
+        <div>Sentiment: </div>
+        <div>Placeholder</div>
+        <div>Score</div>
+        <div>Placeholder</div>
+      </div>
     </div>
   );
 }
